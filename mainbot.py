9be428577c6fef05e6b6f356ad9d3ccffaa9f890
Simon Jorstedt <jorstedtsimon@gmail.com>
2022-07-17 14:13:27 +0200

    Wrote basic functionality in mainbot.py
    Imported necessary libraries, and wrote an on_ready() event response as well as a simple functionality for message interaction.

--- mainbot.py
+++ mainbot.py
@@ -1,1 +1,22 @@
-print("This is printed from the python bot file!")
+import discord
+import os
+
+
+#Create an instance of a client
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('I have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if(message.author == client.user):
+        return
+    
+    if(message.content == '$hello'):
+        await message.channel.send('Hello world!')
+
+
+client.run(os.environ['key3'])
+
